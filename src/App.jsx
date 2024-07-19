ecc3c3c650e9002d65e53ea09b8dd1155332632b
elakiet <elaki.kavin@gmail.com>
2024-07-19 08:47:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import StopWatchComponent from './Components/StopWatchComponent/StopWatchComponent'
+
+function App() {
+  return (
+   <StopWatchComponent/>
+  )
+}
+
+export default App
